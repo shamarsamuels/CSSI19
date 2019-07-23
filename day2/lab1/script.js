12026f06d2537857a7a9b97f439c836c6da1d724
shamarsamuels <shamarsamuels@mec.science>
2019-07-23 16:44:29 -0400

    Update To Javascript Code

--- day2/lab1/script.js
+++ day2/lab1/script.js
@@ -5,8 +5,22 @@ const language = document.getElementById('language')
 const continent = document.getElementById('continent')
 const population = document.getElementById('population')
 const link = document.getElementById('link')
-const flag = document.getElementById('flag')
 const reset = document.getElementById('reset')
+
+const width = '370px'
+const height = '320px'
+const empty = 'https://www.civhc.org/wp-content/uploads/2018/10/question-mark.png'
+
+const images_ids = ['image1', 'image2', 'image3', 'image4', 'image5']
+const images = images_ids.map(el => {
+    el = document.getElementById(el);
+    if (el) {
+        el.style.height = height;
+        el.style.width = width;
+        el.setAttribute('src', empty)
+        return el
+    }
+})
 
 const countries = ['us', 'fr', 'jpn', 'brz']
 const country_info = {
@@ -17,7 +31,13 @@ const country_info = {
         'continent' : 'north america',
         'population' : '327.2 million',
         'link' : 'https://en.wikipedia.org/wiki/United_States',
-        'image' : 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/125px-Flag_of_the_United_States.svg.png',
+        'images' : [
+            'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/125px-Flag_of_the_United_States.svg.png',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Greater_coat_of_arms_of_the_United_States.svg/85px-Greater_coat_of_arms_of_the_United_States.svg.png',
+            'https://i.kinja-img.com/gawker-media/image/upload/s--_FkwptPI--/c_scale,f_auto,fl_progressive,q_80,w_800/asonbeoxc84ux4oklgm6.jpg',
+            'https://i.pinimg.com/originals/d7/0a/db/d70adb89e5a2f9d8fb4c4204eb1e7009.jpg',
+            'https://www.planetware.com/photos-large/USCA/california-golden-gate-bridge-night.jpg',
+        ],
     },
     fr : {
         'country' : 'france',
@@ -26,7 +46,13 @@ const country_info = {
         'continent' : 'europe',
         'population' : '66 million',
         'link' : 'https://en.wikipedia.org/wiki/France',
-        'image' : 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/125px-Flag_of_France.svg.png',
+        'images' : [
+            'https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/125px-Flag_of_France.svg.png',
+            'https://www.vikingrivercruises.com/images/Paris_Effel_Tower_Seine_Panorama_478x345_tcm21-137845.jpg?w=359&h=259',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/France_base_map_18_regions.png/400px-France_base_map_18_regions.png',
+            'https://s.marketwatch.com/public/resources/images/MW-GO787_france_ZH_20180823041143.jpg',
+            'https://www.slh.com/globalassets/country-pages/hero-images/france3.jpg',
+        ],
     },
     jpn : {
         'country' : 'japan',
@@ -35,7 +61,13 @@ const country_info = {
         'continent' : 'asia',
         'population' : '126 million',
         'link' : 'https://en.wikipedia.org/wiki/Japan',
-        'image' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Japan%28bordered%29.svg/125px-Flag_of_Japan%28bordered%29.svg.png',
+        'images' : [
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Japan%28bordered%29.svg/125px-Flag_of_Japan%28bordered%29.svg.png',
+            'https://d36tnp772eyphs.cloudfront.net/blogs/1/2011/05/japan-1200x729.jpg',
+            'https://www.kids-world-travel-guide.com/images/xjapan_map_shutterstock_96744496-2.jpg.pagespeed.ic.3j6Thx5v9p.jpg',
+            'https://d3ertfc829vzop.cloudfront.net/-/media/Tauck/Products/Hero-Images/XJ_18_banner.jpg?h=817&la=en&w=1400&rev=2b50a6fb23c64c7bb1a29714663ff205&hash=EE8E41BE22F0F979EBBD8415EBD8B9208B97ED94',
+            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/01/10/15/japan.jpg?w968h681',
+        ],
     },
     brz : {
         'country' : 'brazil',
@@ -44,7 +76,13 @@ const country_info = {
         'continent' : 'south america',
         'population' : '210 million',
         'link' : 'https://en.wikipedia.org/wiki/Brazil',
-        'image' : 'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/125px-Flag_of_Brazil.svg.png',
+        'images' : [
+            'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/125px-Flag_of_Brazil.svg.png',
+            'https://kids.nationalgeographic.com/content/dam/kids/photos/Countries/A-G/Brazil-Country-Map-UPDT.ngsversion.1466609459209.adapt.710.1.jpg',
+            'https://img.theculturetrip.com/x/smart/wp-content/uploads/2019/04/shutterstock_421013719.jpg',
+            'https://www.bbcgoodfood.com/sites/default/files/guide/guide-image/2014/05/brazil-main.jpg',
+            'https://www.worldatlas.com/r/w728-h425-c728x425/upload/d5/6a/da/shutterstock-765387754.jpg',
+        ],
     },
 }
 
@@ -60,7 +98,9 @@ countries.forEach(country_id => {
             continent.innerHTML = 'continent : ' + info['continent'];
             population.innerHTML = 'population : ' + info['population'];
             link.innerHTML = 'more information : <a target="_" href="' + info['link'] + '">' + info['link'] + '</a>';
-            flag.setAttribute('src', info['image']);
+            info.images.forEach((el, index) => {
+                images[index].setAttribute('src', el)
+            })
         })
     } else {
         console.log('Country Element Does Not Exist');
@@ -74,5 +114,7 @@ reset.addEventListener('click', () => {
     continent.innerHTML = '';
     population.innerHTML = '';
     link.innerHTML = '';
-    flag.setAttribute('src', '');
+    images.forEach(el => {
+        el.setAttribute('src', empty)
+    })
 })
