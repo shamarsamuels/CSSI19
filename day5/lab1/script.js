12d72171089a0fe2251a50d0254f93c848fdba8c
shamarsamuels <shamarsamuels@mec.science>
2019-07-29 11:23:37 -0400

    Update

--- day5/lab1/script.js
+++ day5/lab1/script.js
@@ -1,1 +1,27 @@
 console.log('1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
+
+// let num = prompt('Enter a number?');
+// num = parseInt(num)
+// console.log(num)
+
+// if(!num) {
+//     num = 10
+// }
+// console.log(num + 10)
+
+let response = prompt('What was your grade?');
+let grade = parseInt(response);
+
+if (grade){
+    if(grade >= 70){
+        console.log('You passed!')
+    } else {
+        console.log('You failed.')
+    }
+} else {
+    console.log(response, 'is not a number.')
+}
+
+
+
+
